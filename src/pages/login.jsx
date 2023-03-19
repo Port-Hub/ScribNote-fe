9999b7eb8c82d3fdb757949fea4f9af76ce94432
RahulRST <srahuljayaraj@gmail.com>
2023-03-19 23:17:10 +0530

    icon

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,7 +6,10 @@ const Login = () => {
                 <img src = "/username.png" className="h-10"></img>
                 <input type= "text" className="border-2 border-black rounded-md p-2" placeholder="Username"></input>
             </div>
-            <input type="password" className="border-2 border-black rounded-md p-2" placeholder="Password"></input>
+            <div className="flex flex-row">
+                <img src = "/password.png" className="h-10"></img>
+                <input type="password" className="border-2 border-black rounded-md p-2" placeholder="Password"></input>
+            </div>
             <a className="text-xs underline italic">Forgot password</a>
             <span>Don't have an account? <a className="text-xs underline italic">Sign up</a></span>
             <button className="border-2 p-4 rounded-lg hover:scale-95 active:scale-105">Log in</button>
