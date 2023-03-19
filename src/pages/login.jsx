bc53a765eff1bae0ee4b29519b3e670d2dd1b544
RahulRST <srahuljayaraj@gmail.com>
2023-03-19 22:48:14 +0530

    img

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,13 +2,14 @@ const Login = () => {
     return (
         <div className="flex flex-row h-screen gap-10 items-center justify-around">
         <div className='flex flex-col gap-4 p-10'>
-            <h2 className="text-center font-serif text-xl">ScribeNotes</h2>
-            <p>Username</p>
+            <h2 className="text-center font-serif text-xl">ScribNotes</h2>
             <input type= "text" className="border-2 border-black rounded-md p-2" placeholder="Username"></input>
-            <p>Password</p>
+            <img src = "/username.png object-contain md:object-contain"></img>
             <input type="password" className="border-2 border-black rounded-md p-2" placeholder="Password"></input>
             <a className="text-xs underline italic">Forgot password</a>
+            <a className="text-xs underline italic">Don't have an account? Sign up</a>
             <button>Log in</button>
+  
           </div>
           </div>
     )
