37a0ba7a55786465de0df7516681f2182d5a26ec
RahulRST <srahuljayaraj@gmail.com>
2023-03-19 22:03:35 +0530

    tag

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,9 +4,9 @@ const Login = () => {
         <div className='flex flex-col gap-4 p-10'>
             <h2 className="text-center font-serif text-xl">ScribeNotes</h2>
             <p>Username</p>
-            <textField className="border-2 border-black rounded-md p-2" placeholder="Username"></textField>
+            <input type= "text" className="border-2 border-black rounded-md p-2" placeholder="Username"></input>
             <p>Password</p>
-            <textField className="border-2 border-black rounded-md p-2" placeholder="Password"></textField>
+            <input type="password" className="border-2 border-black rounded-md p-2" placeholder="Password"></input>
             <a className="text-xs underline italic">Forgot password</a>
             <button>Log in</button>
           </div>
