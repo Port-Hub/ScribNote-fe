f1646de9e4041cd69b504c36ce72cc6520c98b8c
Adelin-Brianna <adelinbrianna15@gmail.com>
2023-03-19 22:57:34 +0530

    image alignment

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,16 +1,16 @@
 const Login = () => {
     return (
-        <div className="flex flex-row h-screen gap-10 items-center justify-around">
         <div className='flex flex-col gap-4 p-10'>
             <h2 className="text-center font-serif text-xl">ScribNotes</h2>
-            <input type= "text" className="border-2 border-black rounded-md p-2" placeholder="Username"></input>
-            <img src = "/username.png object-contain md:object-contain"></img>
+            <div className="flex flex-row">
+                <img src = "/username.png" className="h-10"></img>
+                <input type= "text" className="border-2 border-black rounded-md p-2" placeholder="Username"></input>
+            </div>
             <input type="password" className="border-2 border-black rounded-md p-2" placeholder="Password"></input>
             <a className="text-xs underline italic">Forgot password</a>
-            <a className="text-xs underline italic">Don't have an account? Sign up</a>
-            <button>Log in</button>
+            <span>Don't have an account? <a className="text-xs underline italic">Sign up</a></span>
+            <button className="border-2 p-4 rounded-lg hover:scale-95 active:scale-105">Log in</button>
   
-          </div>
           </div>
     )
 }
