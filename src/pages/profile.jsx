4baee76d516ddba187c15279e7150d7d463aa91e
Adelin-Brianna <adelinbrianna15@gmail.com>
2023-03-22 09:53:43 +0530

    badged

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,26 +1,24 @@
 const Profile = () => {
     return (
-        <div>
-            
+      <div>
         <div className='flex flex-col gap-x-4 gap-y-8 p-10 gap-x-4 justify-evenly'>
             <h2 className="text-center font-serif text-xl">PROFILE</h2>
             <div className="flex flex-row gap-2 gap-x-4 justify-between">
                 <p>Username</p>
-                <p placeholder="Username">RAHUL</p>
+                <p className="badge badge-ghost p-4" placeholder="Username">Rahul</p>
+            </div>
+            <div className="flex flex-row gap-2 gap-x-4 justify-between">
+              <p>E-Mail</p>
+              <p className="badge badge-ghost p-4" placeholder="Email">rahuljayaraj@gmail.com</p>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl image-full">
+              <div className="flex flex-col card-body">
+                <h2 className="card-title">PDF Name</h2>
+                <p>Date : 1/1/22</p>
+              </div>
+            </div>
+        </div>
       </div>
-      <div className="flex flex-row gap-2 gap-x-4 justify-between">
-                <p>EMAIL</p>
-                <p placeholder="Email">rahuljeyaraj@gmail.com</p>
-      </div>
-      <div className="card w-96 bg-base-100 shadow-xl image-full">
-  <div className="flex flex-row card-body">
-    <h2 className="card-title">PDF NAME</h2>
-    <p>DATE</p>
-    <div className="card-actions justify-end"></div>
-  </div>
-</div>
-    </div>
-  </div>
     )
 }
  export default Profile;
