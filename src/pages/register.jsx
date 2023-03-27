d5527fc5bfa36137dd9959d3dea9e88b23925c07
Adelin-Brianna <adelinbrianna15@gmail.com>
2023-03-27 22:31:06 +0530

    icon

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,8 +1,15 @@
 import { Link, useNavigate } from "react-router-dom";
-import { faUser, faMailBulk, faKey } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faMailBulk, faKey, faEye, faEyeSlash  } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 const Register = () => {
+    const [ user, setUser ] = useState("");
+    const [ mail, setMail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ show, setShow ] = useState(false);
+    const [ confpassword, setConfpassword ] = useState("");
+    const [ cshow, setCshow ] = useState(false);
     const navigate = useNavigate();
 
     const handleRegister = () => {
@@ -14,19 +21,38 @@ const Register = () => {
             <h2 className="text-center text-primary font-serif text-xl">Create Your <br />ScribNote Account</h2>
             <div className="flex flex-row items-center gap-x-4 justify-between">
                 <FontAwesomeIcon className="text-secondary" icon={faUser} />
-                <input type="text" className="border-2 border-primary rounded-md p-2" placeholder="Username"></input>
+                <input type="text" 
+                onChange={(e) => setUser(e.target.value)}
+                className="border-2 border-primary rounded-md p-2" 
+                placeholder="Username"/>
             </div>
             <div className="flex flex-row items-center gap-x-4  justify-between">
                 <FontAwesomeIcon className="text-secondary" icon={faMailBulk} />
-                <input type="text" className="border-2 border-primary rounded-md p-2" placeholder="Email"></input>
+                <input type="text" 
+                onChange={(e) => setMail(e.target.value)}
+                className="border-2 border-primary rounded-md p-2"
+                placeholder="Email"/>
             </div>
             <div className="flex flex-row items-center gap-x-4  justify-between">
                 <FontAwesomeIcon className="text-secondary" icon={faKey} />
-                <input type="password" className="border-2 border-primary rounded-md p-2" placeholder="Password"></input>
+                <input type={show?"text":"password" }
+                onChange={(e) => setPassword(e.target.value)}
+                className="border-2 border-primary rounded-md p-2" 
+                placeholder="Password"/>
+                <button className="hover:scale-90 active:scale-105" onClick={() => setShow(!show)}>
+                    {show?<FontAwesomeIcon className="text-secondary" icon={faEyeSlash} />:<FontAwesomeIcon className="text-secondary" icon={faEye} />}
+                </button>
             </div>
             <div className="flex flex-row items-center gap-x-4  justify-between">
                 <FontAwesomeIcon className="text-secondary" icon={faKey} />
-                <input type="password" className="border-2 border-primary rounded-md p-2" placeholder="Confirm Password"></input>
+                <input 
+                type={show?"text":"password" }
+                onChange={(e) => setConfpassword(e.taget.value)}
+                className="border-2 border-primary rounded-md p-2" 
+                placeholder="Confirm Password"/>
+                <button className="hover:scale-90 active:scale-105" onClick={() => setShow(!show)}>
+                    {show?<FontAwesomeIcon className="text-secondary" icon={faEyeSlash} />:<FontAwesomeIcon className="text-secondary" icon={faEye} />}
+                </button>
             </div>
             <div className="flex flex-row items-center">
             <button onClick={handleRegister} type="submit"  className="btn btn-outline btn-primary border-2 shadow-xl shadow-sky-900 px-4 py-2 hover:scale-110 active:scale-90">
